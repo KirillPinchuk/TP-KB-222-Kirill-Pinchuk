ddb9016bf5169fbf2e28a69e12e1f6c5c8d6951e
unknown <git@github.com:KirillPinchuk/topic.git>
2023-09-20 14:45:38 +0300

    add solution for topic 01

--- topic_01/task3.py
+++ topic_01/task3.py
@@ -1,7 +1,9 @@
-a = int(input("Enter a: "))
-b = int(input("Enter b: "))
-c = int(input("Enter c: "))
-
-D = b**2 - 4*a*c
-
-print("D = ", D)
+def discriminant(a,b,c):
+    D = b*b-4*a*c
+    return D
+a = int(input("Please enter coef a : "))
+b = int(input("Please enter coef b : "))
+c = int(input("Please enter coef c : "))
+D = discriminant(a,b,c)
+result = discriminant(a,b,c)
+print(result)
