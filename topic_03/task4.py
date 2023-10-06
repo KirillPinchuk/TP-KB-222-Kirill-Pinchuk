85719b029baef559cecf8dc1f956c1ed39e3339c
Кирилл <git@github.com:KirillPinchuk/topic.git>
2023-10-06 11:57:28 +0300

    tipic 3 and task3

--- topic_03/task4.py
+++ topic_03/task4.py
@@ -1,18 +1,11 @@
-def ValuetoIndex(list, NewValue):
-    if NewValue in list:
-        index = list.index(NewValue)
-    else:
-        list.append(NewValue)
-        index = list.index(NewValue)
-    return list, index
-
-list = ["aa", "ee", "zz"]
-print(list)
-
-
+def InsterToPosition(sorted_list, NewValue):
+    position = 0
+    for i in sorted_list:
+     if NewValue < i:
+      position +1
+     return position
+list = ["aa", "ee", "zz"]  
 NewValue = input("New value: ")
-updated_list, index = ValuetoIndex(list, NewValue)
-sortList = sorted(updated_list)
-Newindex = sortList.index(NewValue)
-print("List after sort", sortList)
-print("Index =", Newindex)
+insert_position = InsterToPosition(list, NewValue)
+list.insert(insert_position, NewValue)
+print(f"position: {insert_position}, list: {list}")
